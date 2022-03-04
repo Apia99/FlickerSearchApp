7bb791578a2220fce946c04beb0dbb528d9b237e
Apia99 <98032666+Apia99@users.noreply.github.com>
2022-03-04 19:45:38 +0000

    New App with collection view

---
+++ FlickrSearch/FlickrSearch/Model/FlickrSearchResults.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrSearchResults.swift
+//  FlickrSearch
+//
+//  Created by Admin on 04/03/2022.
+//
+
+import Foundation
+
+struct FlickrSearchResults {
+  let searchTerm: String
+  let searchResults: [FlickrPhoto]
+}
---
+++ FlickrSearch/FlickrSearch/Views/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Admin on 04/03/2022.
+//
+
+import UIKit
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+  @IBOutlet weak var imageView: UIImageView!
+}
